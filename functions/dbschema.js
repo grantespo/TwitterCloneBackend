a27c4c730fc598092299af3064a3ede3a5852789
Grant Espanet <gespanet97@gmail.com>
2019-08-09 18:56:49 -0400

    updated dbschema.js

--- functions/dbschema.js
+++ functions/dbschema.js
@@ -1,36 +1,64 @@
 let db = {
-
-    users: [
-        {
-            //..
-        }
-    ],
-    screams: [
-        {
-            userHandle: 'user',
-            body: 'this is the scream bod',
-            createdAt: '2019-03-15T11:46:01.018Z',
-            likeCount: 5,
-            commentCount: 2
-        }
-    ],
-    comments: [
-        {
-            //...
-        }
-    ]
+  users: [
+    {
+      userId: 'dh23ggj5h32g543j5gf43',
+      email: 'user@email.com',
+      handle: 'user',
+      createdAt: '2019-03-15T10:59:52.798Z',
+      imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
+      bio: 'Hello, my name is user, nice to meet you',
+      website: 'https://google.com',
+      location: 'Boynton Beach, FL'
+    }
+  ],
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'This is a sample scream',
+      createdAt: '2019-03-15T10:59:52.798Z',
+      likeCount: 5,
+      commentCount: 3
+    }
+  ],
+  comments: [
+    {
+      userHandle: 'user',
+      screamId: 'kdjsfgdksuufhgkdsufky',
+      body: 'nice one mate!',
+      createdAt: '2019-03-15T10:59:52.798Z'
+    }
+  ],
+  notifications: [
+    {
+      recipient: 'user',
+      sender: 'john',
+      read: 'true | false',
+      screamId: 'kdjsfgdksuufhgkdsufky',
+      type: 'like | comment',
+      createdAt: '2019-03-15T10:59:52.798Z'
+    }
+  ]
 };
 const userDetails = {
-    //Redux data
-    credentials: {
-        //...
+  // Redux data
+  credentials: {
+      userId: 'dh23ggj5h32g543j5gf43',
+      email: 'user@email.com',
+      handle: 'user',
+      createdAt: '2019-03-15T10:59:52.798Z',
+      imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
+      bio: 'Hello, my name is user, nice to meet you',
+      website: 'https://google.com',
+      location: 'Boynton Beach, FL'
+  },
+  likes: [
+    {
+      userHandle: 'user',
+      screamId: 'hh7O5oWfWucVzGbHH2pa'
     },
-    likes: [
-        {
-            //..
-        },
-        {
-            //..
-        }
-    ]
-}
+    {
+      userHandle: 'user',
+      screamId: '3IOnFoQexRcofs5OhBXO'
+    }
+  ]
+};
